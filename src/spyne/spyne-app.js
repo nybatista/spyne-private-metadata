ba44bbb4fd15b4ffee08e430e7916697c55a40f2
Frank Batista <nybatista@gmail.com>
2024-12-27 03:02:35 -0500

    consolidating changes made during npm package and travis ci testing

--- src/spyne/spyne-app.js
+++ src/spyne/spyne-app.js
@@ -5,7 +5,7 @@ import { SpyneAppProperties } from './utils/spyne-app-properties'
 import { sanitizeHTMLConfigure } from './utils/sanitize-html'
 
 const _channels = new ChannelsMap()
-const version = '0.20.0'
+const version = '0.20.2'
 
 class SpyneApplication {
   /**
@@ -42,7 +42,7 @@ class SpyneApplication {
   init(config = {}, testMode = false) {
     // this.channels = new ChannelsMap();
     /*!
-     * Spyne 0.20.0
+     * Spyne 0.20.2
      * https://spynejs.org
      *
      * @license Copyright 2017-2021, Frank Batista, Relevant Context, LLC. All rights reserved.
